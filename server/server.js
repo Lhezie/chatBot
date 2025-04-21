a4edf4328507164cc6378c03b7c36e8d598ab1a1
Elizabeth Enechaziam <lizabethbunor@gmail.com>
2025-04-21 10:34:06 +0100

    update

--- server/server.js
+++ server/server.js
@@ -23,7 +23,8 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = parseInt(process.env.PORT, 10) || NEXT_PUBLIC_API_URL;
+const PORT = parseInt(process.env.PORT, 10) || 4000;
+
 if (isNaN(PORT)) {
   throw new Error(`Invalid PORT value in .env: ${process.env.PORT}`);
 }
